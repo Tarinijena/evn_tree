2babd86ba5a7a6ecd168b110a01d3f2b3302ee36
Tarinijena <130827816+Tarinijena@users.noreply.github.com>
2024-09-11 21:36:33 +0530

    by tarini

--- lib/app/module/splash_screen/splash_screen.dart
+++ lib/app/module/splash_screen/splash_screen.dart
@@ -2,7 +2,10 @@ import 'dart:async';
 import 'dart:convert';
 
 import 'package:flutter/material.dart';
+import 'package:national_wild_animal/app/api_service/api_end_point.dart';
+import 'package:national_wild_animal/app/api_service/http_methods.dart';
 import 'package:national_wild_animal/app/app_utils/helper.dart';
+import 'package:national_wild_animal/app/module/splash_screen/refresh_token_model.dart';
 
 import '../../app_utils/shared_preferance.dart';
 
@@ -14,10 +17,54 @@ class SplashScreen extends StatefulWidget {
 }
 
 class _SplashScreenState extends State<SplashScreen> with Helper {
+
   SharedPref sharedPref = SharedPref();
+  Future<bool> getRefreshToken() async {
+  Completer<bool> completer = Completer<bool>();
+
+  try {
+    // Retrieve the stored JSON string from shared preferences
+    String jsonData = await sharedPref.getKey("userResponse"); 
+    
+    print(jsonData);
+
+    // Decode the JSON string into a Map and then parse it to UserResponse
+    Map<String, dynamic> jsonMap = json.decode(jsonData);
+    UserResponse userResponse = UserResponse.fromJson(jsonMap);
+
+    // Extract the token from the UserResponse object
+    String? token = userResponse.data?.token;
+
+    if (token != null) {
+      // Call the API method using the token
+      HttpMethodsDio().getMethodWithToken(
+        api: ApiEndPoint.getRefreshToken,
+        fun: (map, code) {
+          if (code == 200 && map['data'] != null && map['data'].isNotEmpty) {
+            // Handle successful token refresh
+            completer.complete(true);
+          } else {
+            completer.complete(false);  // Handle other scenarios
+          }
+        },
+        token: token, // Pass the extracted token
+      );
+    } else {
+      completer.complete(false);  // Handle case where token is null
+    }
+  } catch (e) {
+    completer.completeError(e);  // Complete with an error if something goes wrong
+  }
+
+  return completer.future;
+}
+
+
+ 
   @override
   void initState() {
     super.initState();
+    getRefreshToken();
 
     Future.delayed(Duration(seconds: 3), () {
       setRoute();
